09c25cb4d2a5e1f30f556c5ee3892c6c98504033
Nyako <24845294+nyakowint@users.noreply.github.com>
2024-01-09 17:05:54 -0500

    yeast

--- native.ts
+++ native.ts
@@ -7,7 +7,8 @@
 import { User } from "discord-types/general";
 import { app, WebFrameMain } from "electron";
 import { createServer, Server as HttpServer } from "http";
-import { Server, Socket } from "socket.io";
+
+import { Server, Socket } from "./dependencies.dist";
 
 let io: Server;
 let httpServer: HttpServer;
